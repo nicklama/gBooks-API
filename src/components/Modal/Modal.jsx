54824e0e6d671eb7765141e5bc092048288a2acd
Nick Lamattina <nicklamattina@outlook.com.au>
2022-04-12 18:06:00 +1000

    Added background click to Modal

--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -1,39 +1,37 @@
 import style from "./Modal.module.scss";
 
 const Modal = ({ selectedBook, handleModalClick }) => {
+    // toggles the modal when a click occurs outside the modal
+    const handleBackgroundClick = (e) => {
+        if (e.target.className === style.Background) handleModalClick();
+    };
+
     return (
-        <div className={style.Modal}>
-            <span className={style.Modal__Close} onClick={handleModalClick}>
-                &times;
-            </span>
-            <img
-                className={style.Modal__Image}
-                src={
-                    selectedBook.volumeInfo.imageLinks
-                        ? selectedBook.volumeInfo.imageLinks.thumbnail
-                        : "http://lgimages.s3.amazonaws.com/nc-sm.gif"
-                } // placeholder image if the thumbnail is undefined
-                alt="Book Cover"
-            />
-            <span className={style.Modal__Content}>
-                <h3>{selectedBook.volumeInfo.title}</h3>
-                <p>{selectedBook.volumeInfo.subtitle}</p>
-                {/* Checks if the authors array exists before and does not render if undefined */}
-                {selectedBook.volumeInfo.authors ? (
-                    <p>By: {selectedBook.volumeInfo.authors.join(", ")}</p>
-                ) : (
-                    <></>
-                )}
-                <p>Released: {selectedBook.volumeInfo.publishedDate}</p>
-                {/* Also checks categories exists to prevent an undefined error when calling .join() */}
-                {selectedBook.volumeInfo.categories ? (
-                    <p>Categories: {selectedBook.volumeInfo.categories.join(", ")}</p>
-                ) : (
-                    <></>
-                )}
-                <p>Language: {selectedBook.volumeInfo.language.toUpperCase()}</p>
-                <p>{selectedBook.volumeInfo.description}</p>
-            </span>
+        <div className={style.Background} onClick={handleBackgroundClick}>
+            <div className={style.Modal}>
+                <span className={style.Modal__Close} onClick={handleModalClick}>
+                    &times;
+                </span>
+                <img
+                    className={style.Modal__Image}
+                    src={
+                        selectedBook.volumeInfo.imageLinks?.thumbnail ??
+                        "http://lgimages.s3.amazonaws.com/nc-sm.gif"
+                    } // placeholder image if the thumbnail is undefined
+                    alt="Book Cover"
+                />
+                <span className={style.Modal__Content}>
+                    <h3>{selectedBook.volumeInfo.title}</h3>
+                    <p>{selectedBook.volumeInfo.subtitle}</p>
+                    {/* Checks if the authors array exists before and does not render if undefined */}
+                    <p>By: {selectedBook.volumeInfo.authors?.join(", ")}</p>
+                    <p>Released: {selectedBook.volumeInfo.publishedDate}</p>
+                    {/* Also checks categories exists to prevent an undefined error when calling .join() */}
+                    <p>Categories: {selectedBook.volumeInfo.categories?.join(", ")}</p>
+                    <p>Language: {selectedBook.volumeInfo.language.toUpperCase()}</p>
+                    <p>{selectedBook.volumeInfo.description}</p>
+                </span>
+            </div>
         </div>
     );
 };
